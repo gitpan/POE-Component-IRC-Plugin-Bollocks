92ee36111924d9a81646f5ed04dd62b23545b724
Chris Williams <chris@bingosnet.co.uk>
2008-05-12 13:55:39 +0000

    Import of BINGOS/POE-Component-IRC-Plugin-Bollocks-0.06 from CPAN.
    gitpan-cpan-distribution: POE-Component-IRC-Plugin-Bollocks
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         BINGOS/POE-Component-IRC-Plugin-Bollocks-0.06.tar.gz
    gitpan-cpan-author:       BINGOS
    gitpan-cpan-maturity:     released

--- lib/POE/Component/IRC/Plugin/Bollocks.pm
+++ lib/POE/Component/IRC/Plugin/Bollocks.pm
@@ -6,7 +6,7 @@ use Dev::Bollocks;
 use POE::Component::IRC::Plugin qw(:ALL);
 use vars qw($VERSION);
 
-$VERSION = '0.04';
+$VERSION = '0.06';
 
 my @phrases = (
   "So, let's ", 'We can ', 'We should ', 'Our mission is to ',
@@ -130,6 +130,8 @@ random management bollocks to the channel.
 Chris C<BinGOs> Williams <chris@bingosnet.co.uk>
 
 =head1 LICENSE
+
+Copyright C<(c)> Chris Williams.
 
 This program is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
 
